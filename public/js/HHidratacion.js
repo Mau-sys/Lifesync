93aa5e20178248fdbd13112d8d307c1088ca6019
Josue-Corea <josue.corea2027@frech.superate.org.sv>
2026-07-30 16:38:40 -0600

    kebap

---
+++ public/js/HHidratacion.js
@@ -0,0 +1,19 @@
+const btnOptions = document.getElementById('btn-options-academico');
+const kebabMenu = document.getElementById('kebab-menu-academico');
+const btnRegresar = document.getElementById('btn-regresar');
+
+btnOptions.addEventListener('click', (e) => {
+    e.stopPropagation();
+    kebabMenu.classList.toggle('show');
+});
+
+document.addEventListener('click', (e) => {
+    if (!kebabMenu.contains(e.target)) {
+        kebabMenu.classList.remove('show');
+    }
+});
+
+btnRegresar.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.history.back();
+});
